4bd179330e3787a0ed9257208853def67c105558
Natanael de Holanda e Oliveira <natan.holanda@gmail.com>
2024-10-23 14:50:20 -0300

    Assisti até a aula 50, Parametros tipo de referencia pt2

---
+++ src/javacore/Bintroducaometodos/dominio/ImpressoraEstudante.java
@@ -0,0 +1,7 @@
+package javacore.Bintroducaometodos.dominio;
+
+public class ImpressoraEstudante {
+    public void imprime(Estudante estudante) {
+        System.out.println(estudante.toString());
+    }
+}
